8022cf364c5d61c1d7caa4cf07734b8d2c0d26df
Stevo <nsanzestevo1@gmail.com>
2024-11-22 13:07:17 +0200

    Ensure BookList view uses generics.ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
-from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
