7d8685c6805891a2bd507ee80615689ccdb5ed7c
Stevo <nsanzestevo1@gmail.com>
2024-12-22 16:27:50 +0200

    Fixed URL paths for add_book and edit_book

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('add_book/', views.add_book, name='add_book'),  # Corrected path
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),  # Corrected path
     path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', views.CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
