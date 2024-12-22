b6f4726524c8fd5639a18bc3d1807c065f1b328b
Stevo <nsanzestevo1@gmail.com>
2024-12-22 16:04:32 +0200

    Added role-based views, user profile handling, and tests

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from . import views  # Import views directly
+from . import views
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', views.CustomLoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', views.CustomLogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', views.register_view, name='register'),  # Corrected URL pattern for registration
+    path('register/', views.register_view, name='register'),
 ]
