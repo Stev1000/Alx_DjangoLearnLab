0c1647006dffc41912269d930b17f2e35e2d275a
Stevo <nsanzestevo1@gmail.com>
2024-12-30 17:48:56 +0200

    Refined static file handling, URLs, and profile views with CSRF protection

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,10 +3,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),  
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import CustomUserCreationForm, ProfileUpdateForm
 from django.http import HttpResponse
-from .forms import UserProfileForm 
+#from .forms import UserProfileForm 
+from django.contrib import messages
+from .forms import ProfileUpdateForm
 
 def home(request):
     return HttpResponse("Welcome to the Django Blog!")
@@ -23,25 +25,41 @@ def register(request):
     return render(request, 'register.html', {'form': form})
 
 # Profile view
-@login_required
 def profile(request):
+    """Display and edit profile information for authenticated users."""
     if request.method == 'POST':
-        u_form = ProfileUpdateForm(request.POST, instance=request.user)
-        if u_form.is_valid():
-            u_form.save()
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
             messages.success(request, 'Your profile has been updated!')
             return redirect('profile')
     else:
-        u_form = ProfileUpdateForm(instance=request.user)
+        form = ProfileUpdateForm(instance=request.user)
 
-    return render(request, 'registration/profile.html', {'u_form': u_form})
-def edit_profile(request):
+    return render(request, 'registration/profile.html', {'form': form})
+
+@login_required
+def profile(request):
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=request.user)
+        form = ProfileUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('profile')  # Make sure 'profile' is a valid URL name
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
     else:
-        form = UserProfileForm(instance=request.user)
-    
-    return render(request, 'blog/edit_profile.html', {'form': form})
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'registration/profile.html', {'form': form})
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully!')
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'registration/edit_profile.html', {'form': form})
