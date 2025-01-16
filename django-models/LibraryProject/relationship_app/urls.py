0265be7c74c727dc85c70787e0f516c4d0879463
Stevo <nsanzestevo1@gmail.com>
2025-01-16 14:57:59 +0200

    Implemented user authentication with login, logout, and registration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 
     # Authentication views
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Use LogoutView
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', register, name='register'),
 ]
