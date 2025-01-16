7109a9b893a284353af8a9178edd4852a7ff06e4
Stevo <nsanzestevo1@gmail.com>
2025-01-16 15:15:13 +0200

    Implement user authentication with login, logout, and registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.contrib import messages
 from .models import Library, Book
 
@@ -20,9 +21,10 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)  # Log in the user after successful registration
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f"Welcome, {username}! Your account has been created.")
             return redirect('login')
     else:
         form = UserCreationForm()
