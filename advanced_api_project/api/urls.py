7a1830a12026e78e132fba3859c52163f51283c0
Stevo <nsanzestevo1@gmail.com>
2025-01-22 12:18:43 +0200

    Deleted the advanced_api_project directory

--- advanced_api_project/api/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, BookViewSet
-
-router = DefaultRouter()
-router.register('authors', AuthorViewSet, basename='author')
-router.register('books', BookViewSet, basename='book')
-
-urlpatterns = router.urls
