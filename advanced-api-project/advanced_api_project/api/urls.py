c4eaa532aa80e8ec4cf003e6dbcee1d679f29e3d
Stevo <nsanzestevo1@gmail.com>
2025-01-22 09:27:41 +0200

    Initial commit: Refined Django project for advanced API development

--- advanced-api-project/advanced_api_project/api/urls.py
+++ advanced-api-project/advanced_api_project/api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import AuthorListView, BookListView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet, AuthorViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+router.register(r'authors', AuthorViewSet, basename='author')
 
 urlpatterns = [
-    path('authors/', AuthorListView.as_view(), name='author-list'),
-    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,13 +1,20 @@
 from rest_framework.generics import ListAPIView
-from .models import Author, Book
-from .serializers import AuthorSerializer, BookSerializer
+from rest_framework import viewsets
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
+from rest_framework.permissions import IsAuthenticated
+#from .permissions import IsAdminOrReadOnly
 
-# View for listing all authors
-class AuthorListView(ListAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
-# View for listing all books
-class BookListView(ListAPIView):
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
